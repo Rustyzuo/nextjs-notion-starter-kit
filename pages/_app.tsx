0e7dbff2d71e7be108d8f2a5ce509d7114ca5d4c
Rustyzuo <135428502+Rustyzuo@users.noreply.github.com>
2025-12-11 07:36:59 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,10 @@
 // pages/_app.tsx
-
-// 1. 用于公式渲染 (可选)
 import 'katex/dist/katex.min.css'
-
-// 2. 代码高亮主题设置
-// 原本的亮色主题被我注释掉了，改用了适合暗黑模式的 okaidia 主题
-// import 'prismjs/themes/prism-coy.css' 
-import 'prismjs/themes/prism-okaidia.css' 
-
-// 3. 核心样式 (必须)
+// 使用适合暗黑模式的代码高亮主题
+import 'prismjs/themes/prism-okaidia.css'
 import 'react-notion-x/src/styles.css'
-
-// 4. 全局样式
 import 'styles/global.css'
-
-// 5. Notion 样式覆盖
 import 'styles/notion.css'
-
-// 6. Prism 代码高亮样式覆盖
 import 'styles/prism-theme.css'
 
 import type { AppProps } from 'next/app'
@@ -25,7 +12,7 @@ import * as Fathom from 'fathom-client'
 import { useRouter } from 'next/router'
 import { posthog } from 'posthog-js'
 import * as React from 'react'
-// 引入主题管理组件
+// 引入 next-themes
 import { ThemeProvider } from 'next-themes'
 
 import { bootstrap } from '@/lib/bootstrap-client'
@@ -46,33 +33,22 @@ export default function App({ Component, pageProps }: AppProps) {
 
   React.useEffect(() => {
     function onRouteChangeComplete() {
-      if (fathomId) {
-        Fathom.trackPageview()
-      }
-
-      if (posthogId) {
-        posthog.capture('$pageview')
-      }
+      if (fathomId) { Fathom.trackPageview() }
+      if (posthogId) { posthog.capture('$pageview') }
     }
 
-    if (fathomId) {
-      Fathom.load(fathomId, fathomConfig)
-    }
-
-    if (posthogId) {
-      posthog.init(posthogId, posthogConfig)
-    }
+    if (fathomId) { Fathom.load(fathomId, fathomConfig) }
+    if (posthogId) { posthog.init(posthogId, posthogConfig) }
 
     router.events.on('routeChangeComplete', onRouteChangeComplete)
-
     return () => {
       router.events.off('routeChangeComplete', onRouteChangeComplete)
     }
   }, [router.events])
 
   return (
-    // 这里是关键修改：强制默认使用 dark 模式，并且不跟随系统设置
-    <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false}>
+    // 强制默认暗黑模式
+    <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false} value={{ dark: 'dark-mode', light: 'light-mode' }}>
       <Component {...pageProps} />
     </ThemeProvider>
   )
