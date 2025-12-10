e34a63de605e700e0fa921b4f73e93a9b80ab925
Rustyzuo <135428502+Rustyzuo@users.noreply.github.com>
2025-12-11 07:54:47 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,16 @@
-// used for rendering equations (optional)
 import 'katex/dist/katex.min.css'
-// used for code syntax highlighting (optional)
-// import 'prismjs/themes/prism-coy.css' // [修改] 注释掉这个亮色主题
-// core styles shared by all of react-notion-x (required)
+import 'prismjs/themes/prism-okaidia.css'
 import 'react-notion-x/src/styles.css'
-// global styles shared across the entire site
 import 'styles/global.css'
-// this might be better for dark mode
-import 'prismjs/themes/prism-okaidia.css' // [修改] 开启这个暗色主题，配合 Dark Mode
-// global style overrides for notion
 import 'styles/notion.css'
-// global style overrides for prism theme (optional)
 import 'styles/prism-theme.css'
 
-import type { AppProps } from 'next/app'
 import * as Fathom from 'fathom-client'
+import { ThemeProvider } from 'next-themes'
+import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
 import { posthog } from 'posthog-js'
 import * as React from 'react'
-import { ThemeProvider } from 'next-themes' // [修改] 新增引入
 
 import { bootstrap } from '@/lib/bootstrap-client'
 import {
@@ -63,7 +55,6 @@ export default function App({ Component, pageProps }: AppProps) {
   }, [router.events])
 
   return (
-    // [修改] 添加 ThemeProvider 包裹，强制默认暗黑模式
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
       <Component {...pageProps} />
     </ThemeProvider>
